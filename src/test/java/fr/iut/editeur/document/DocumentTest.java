62a6ad4b07ad929bde0777c9ab2c85b98d400012
michauxa <alexis.michaux-kinet@etu.umontpellier.fr>
2024-09-06 11:38:02 +0200

    Github ex2

---
+++ src/test/java/fr/iut/editeur/document/DocumentTest.java
@@ -0,0 +1,14 @@
+package fr.iut.editeur.document;
+
+import org.junit.jupiter.api.Test;
+
+  import static org.junit.jupiter.api.Assertions.*;
+
+  public class DocumentTest {
+
+      @Test
+      public void testMethode() {
+          assertXXX(...)
+      }
+
+  }
